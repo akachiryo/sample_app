ab5d08af7696ce0c3f68284e559373cb4639b6d5
akachiryo <rirugameri@icloud.com>
2021-12-20 19:28:27 +0900

    mail finish

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,6 +16,11 @@ class UserMailer < ApplicationMailer
     mail to: user.email
   end
 
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+  end
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
